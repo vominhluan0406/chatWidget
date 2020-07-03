df68e90228c9ff3573825edcd84ec98d50405168
Luan <vominhluan0406@gmail.com>
2020-07-03 22:08:55 +0700

    3/7-22:08

--- Webchat/js/control.js
+++ Webchat/js/control.js
@@ -3,15 +3,15 @@ while (localStorage.getItem(name) == undefined) {
     alert('Tên không có trong dữ liệu!')
     name = prompt('Nhập tên của bạn: ');
 }
-document.getElementById('user').innerHTML = 'User: '+name;
+document.getElementById('user').innerHTML = 'User: ' + name;
 var value = JSON.parse(localStorage.getItem(name)); //thông tin người dùng(image,friend)
 
 var client = new WebSocketClient({
     onMessage: function (evt) {
         var data = JSON.parse(evt.data);
         console.log(data);
-        $('#' +data.Sender+'_'+ data.Receive).myWidget({ message: [{ message: data.message, Sender: data.Sender }], new: data.new });
-        $('#' +data.Receive+'_'+data.Sender).myWidget({ message: [{ message: data.message, Sender: data.Sender }], new: data.new });
+        $('#' + data.Sender + '_' + data.Receive).myWidget({ message: [{ message: data.message, Sender: data.Sender }], new: data.new });
+        $('#' + data.Receive + '_' + data.Sender).myWidget({ message: [{ message: data.message, Sender: data.Sender }], new: data.new });
     }
 });
 
@@ -23,7 +23,19 @@ $(document).ready(function () {
         return function () {
             var mess = document.getElementById('inp1' + receiver).value; //nội dung
             document.getElementById('inp1' + receiver).value = '';
-
+            var key =sender + ' ' + receiver;
+            var value = localStorage.getItem(sender + ' ' + receiver);
+            if (value == undefined) {
+                value = localStorage.getItem(receiver + ' ' + sender);
+                key = receiver + ' ' + sender;
+            }
+            value = JSON.parse(value);
+            var obj ={
+                'message':mess,
+                'Sender':sender
+            }
+            value.push(obj);
+            localStorage.setItem(key,JSON.stringify(value));
             //Tạo một dòng tin nhắn
             client.sendMessage({ message: mess, 'Sender': sender, new: true, Receive: receiver });
         }
@@ -48,9 +60,9 @@ $(document).ready(function () {
         //Click để mở tin nhắn
         function addChatBox(sender, mess, receiver, senVal, receiverVal) {
             return function () {
-                if ($('#' +sender+"_"+ receiver).length == 0) {  //Kiểm tra chatbox
-                    $('<div>', { id: sender+"_"+ receiver}).appendTo($('#bottom'));
-                    $('#' + sender+"_"+ receiver).myWidget({
+                if ($('#' + sender + "_" + receiver).length == 0) {  //Kiểm tra chatbox
+                    $('<div>', { id: sender + "_" + receiver }).appendTo($('#bottom'));
+                    $('#' + sender + "_" + receiver).myWidget({
                         friend: receiver,
                         img: senVal.image,
                         currentUser: sender,
